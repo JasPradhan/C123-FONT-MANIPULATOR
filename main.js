882ea1d9095cbbe7f3b1809ef997daa169a36233
JasPradhan <70465360+JasPradhan@users.noreply.github.com>
2020-09-26 15:05:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+rightWristX=0;
+
+difference=0;
+
+leftWristX=0;
+
+noseX=0;
+
+noseY=0;
+
+texts=0;
+
 function preload() {
 
 }
@@ -18,12 +30,33 @@ function modelLoaded() {
 
 function draw() {
     background('#0069d1');
+    document.getElementById("font_size").innerHTML = "Font size of the text will be = " + difference +"px";
+    textSize(difference);
+    fill('#FFE787');
+    text(texts, noseX, noseY);
 }
 
-function gotPoses(results) {
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
 
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
 
-    if (results.length > 0) {
-        console.log(results);
-    }
+    texts=document.getElementById("text").value;
+
+  
+          noseX=results[0].pose.nose.x;
+          noseY=results[0].pose.nose.y;
+  
+          console.log("noseX= "+noseX+", noseY= "+noseY);
+
+    console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX + " difference = " + difference);
+  }
 }
+
+
+/*speech*/
